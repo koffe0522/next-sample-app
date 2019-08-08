ffe36ea4aec42918ac81d5c1cb12d16697cd17dc
koffe0522 <kouhei19920522@gmail.com>
2019-08-08 16:34:33 +0900

    コミットもれ

--- credentials/client.js
+++ credentials/client.js
@@ -1,3 +1,9 @@
 module.exports = {
-  // TODO firebase client config
+  apiKey: "AIzaSyAhrSpgkLBXxICJyMOKGN-GdwIFP8EMJew",
+  authDomain: "next-sample-app-25deb.firebaseapp.com",
+  databaseURL: "https://next-sample-app-25deb.firebaseio.com",
+  projectId: "next-sample-app-25deb",
+  storageBucket: "",
+  messagingSenderId: "48133534449",
+  appId: "1:48133534449:web:adcd8c3619070483"
 };
--- credentials/server.js
+++ credentials/server.js
@@ -1,3 +1,16 @@
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
-  // TODO firebase server config
+  type: "service_account",
+  project_id: "next-sample-app-25deb",
+  private_key_id: "3ac8439432e9304bf6ae2dcb9dbb773447d756ee",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQD4y41v3W7x8M7/\nIDI13fDaDOhXtd9pwAJZ2o/LYfsFX8B1GAN4uo3ocl3aIzJNZpMdIT8QS+Zdcu8l\npXys+tJ+6JxmNPTJD1UL6IMr8mZfL0xhScelwRBc5lHBqEZg+3thGql/yXEA9npn\nVGxbcXX9T4q/iXZDprsfqIm2idwTWXjYuxIOs8IUjA/v7fPO5CCk8+bokOZKxLjT\nrcYZsksz4zFD7ro+XyvQyJIJh+v4XQLzifxLY5gLV2nCy800zRhmlItYjmO/fW6m\nDyiI0d0zTaHMS5iZt3wAw6nnc7YPr1d6zcVC/57y/hW9iQ0P7C2/RYfWFFLLa37J\nt/xcJWf9AgMBAAECggEASlWfC6ZQaK83WZjIB7opJS0oGlS3zMPWr/wSbIT42DE0\nsoebOe4bSjEpg2xysheMqu5tBjR3PsKBW3EnYBOM4ugjGpKF9LDFC0rMLkw9dAEQ\nxBykLw0RE4CpOsBJTt60jV3G+Ow5/Dyoc8kQmv26cBXc8b9je3EMhbf4dXaZL5o8\nJv/2HINDTtNUcsopYMhWiTQMP2aofusdNBQ3JVQN03KdZ7wk0FvlunWfbqdjo5/F\nvSL5fFrajVfmyVW261RmAL9bIC1ItF60IXI7/3xDss6I16p8zMP3sEen1kDQ0FBJ\nY/Pi7UTba1uvRlDG6EFIFqDs9ip6lB8GKoc+4b4yLQKBgQD9CqrK57sJQ3MkHwv1\n0a6sPkl57XfcCe2Rd5Am/qH59suNymrJ2VZaCdYEXVXziBfZ/h9Fp9AcDModrn3K\nETNjR/B/rUGo3dbuMAfgI2BCcNZpomPzDnu3jEkvqveZpL05yKWnY49CbXlvxI++\nYqdFZA8RoYdLb8nesy5B0b/lOwKBgQD7tCz/9WqmdbqGJtlIYVvL2GNPlblagaPI\n6VWgjMByZos9ak2izr2eA04ZJ4lkTExpSI65UJd1xMJm007Oc1+JkZGZl6hJvpSz\noV0YFCxIyQ1JQwCf8efkdOfGFELOpwuPyeiKNtPiTc8VNI0KGpPbPo34f6DnhzmU\nWoD6FHS0JwKBgQClNOW1e/4dWjA+kRaZ88B7LF+Ef4oV21WD9+/hxbWzIXJcrJ90\nEbXTK0DTsKyBR4VrCgMY0yuIZMFBB7+QwhIu67UVZuTLf6rn7+v0ZqRePJWAekmP\nUxFrEOhnetQVN/1jUKD23IWX9Smn00mWH4rePymndTKECgRepdW5m1wSKQKBgQDT\n8RMzM+Np5xf4fh3X5KL3Mm3tRZWdsCvJsTB40Av8SHxWanyU6qCfvQi2BaSa3+Q3\ngvEq+cGPWO6YxrVFAcEghYKkYh9CeUedOfK5o8LmRuT5G4IbLAanSAkhsU3P1LjX\nipxURXaEyB/nKyjXqSl3I2VIKa/DRQom8U5JRZA/YQKBgAjM7GoFTeBr4W/O2AB1\nVXBavn81HyOolfjb8pN0N07kYwwD3MrEaBxfZvTN604W2+2w00DOWzs+yc9S1RMR\n+aRglwc8/LTtQCPXln5Xpy3poUtxXMYkjfZvHz3yzj1ESpVv5s90yQOESHRBul4L\nQf8oEFO7NVxZvvCuu9Lllpp9\n-----END PRIVATE KEY-----\n",
+  client_email:
+    "firebase-adminsdk-c6bz0@next-sample-app-25deb.iam.gserviceaccount.com",
+  client_id: "115211488560130664099",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-c6bz0%40next-sample-app-25deb.iam.gserviceaccount.com"
 };
