391d9f8b45bc6e94f481eb82a084f67c122d17b3
Kouhei Kishigami <34618256+koffe0522@users.noreply.github.com>
2020-07-20 13:15:40 +0900

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: "AIzaSyAhrSpgkLBXxICJyMOKGN-GdwIFP8EMJew",
-  authDomain: "next-sample-app-25deb.firebaseapp.com",
-  databaseURL: "https://next-sample-app-25deb.firebaseio.com",
-  projectId: "next-sample-app-25deb",
+  apiKey: "XXXX",
+  authDomain: "XXXX",
+  databaseURL: "XXXX",
+  projectId: "XXXX",
   storageBucket: "",
-  messagingSenderId: "48133534449",
-  appId: "1:48133534449:web:adcd8c3619070483"
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 export default config;
